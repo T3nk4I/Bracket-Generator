4b3201b86b01c3a0c1f35b655037fe8b3070d591
Tenkai Tonatiuh Cruz Sato <finnandjake1010@gmail.com>
2025-01-07 19:44:02 -0600

    Added styles.css, now uses pure css intead of bootstrap, added sidebar, navbar and footer, can access table settings from a modal pop up:

--- groupPhase.js
+++ groupPhase.js
@@ -13,6 +13,31 @@ let tableArray = names.map(name => ({
 
 console.clear();
 console.log(tableArray);
+var modal = document.getElementById("modalSettings");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnSettings");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 document.getElementById('select-num').addEventListener("change", function(){
     const chunkSize = parseInt(this.value);
     generateGroups(chunkSize);
@@ -26,15 +51,18 @@ function generateGroups(chunkSize){
     for (let i = 0; i < tableArray.length; i += chunkSize){
         const chunk = tableArray.slice(i, i + chunkSize);
         console.log(chunk);
-        tableBody.innerHTML += `<tr><td colspan="4" style="background-color:DodgerBlue; color:white; text-align:center;">Grupo ${groupNumber}</td></tr>`;
+        tableBody.innerHTML += `<tr><td colspan="4" style="background-color:#EB5E28; color:white; text-align:center;">Grupo ${groupNumber}</td></tr>`;
         buildTable(chunk);
-        tableBody.innerHTML += `<button id="matches">Show Matches</button>`;
+        tableBody.innerHTML += `<button id="show" type="button" onclick="showMatchNumber()">Show Matches</button>`;
         groupNumber++;
     }
 }
 
-function showMatches(i){
-    tableBody.innerHTML += `<button id="matches">Show Matches</button>`;
+//0.5n(n-1) equation for how many matches in a group of n contestant
+function showMatchNumber (){
+    let n = parseInt(document.getElementById('select-num').value);
+    const matches = 0.5*n*(n-1);
+    alert("The number of matches in this group is", matches);
 }
 
 function buildTable(data){
